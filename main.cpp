d5e69f939cac6db9ce1beec094dfd76e9a53e4c6
Dinuka <dinukagayashankasthuriarachchi@gmail.com>
2024-03-02 00:30:33 +0530

    extract components

--- main.cpp
+++ main.cpp
@@ -1,16 +1,113 @@
-#include <iostream>
-#include <vector>
 #include <fstream>
-#include <string>
 #include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
 
+#include "Error_check.h"
+#include "Staff.h"
 #include "Stock.h"
 #include "Supply.h"
-#include "Staff.h"
-#include "Error_check.h"
 #include "User_interface.h"
 
 using namespace std;
+
+void search_stock_item_by_id()
+{
+	vector<Stock> all_stock;
+	for (int ii = 1; ii < 11; ii++)
+	{
+		vector<Stock> temp = Stock::read_data(ii);
+		for (int ij = 0; ij < temp.size(); ij++)
+			all_stock.push_back(temp[ij]);
+	}
+	bool not_found = 0;
+	string id;
+	Stock item;
+	cout << "\nEnter item ID\t\t: ";
+	getline(cin, id);
+	to_upper(id, 1);
+	item = Stock::find_by_id(id, all_stock, not_found);
+	if (not_found)
+		display_error("SD01");
+	else
+		item.show_data();
+}
+
+void search_stock_item_by_name()
+{
+	vector<Stock> all_stock;
+	for (int ii = 1; ii < 11; ii++)
+	{
+		vector<Stock> temp = Stock::read_data(ii);
+		for (int ij = 0; ij < temp.size(); ij++)
+			all_stock.push_back(temp[ij]);
+	}
+	string name;
+	cout << "\nEnter item Name\t\t: ";
+	getline(cin, name);
+	to_upper(name, 1);
+	vector<Stock> items = Stock::find_item(name, 1);
+	if (items.size() == 0)
+		display_error("SD02");
+	else if (items.size() == 1)
+		items[0].show_data();
+	else
+	{
+		Stock item;
+		Stock::find_name_and_display(item, all_stock, name, 1);
+		item.show_data();
+	}
+}
+
+void search_stock_item_by_category()
+{
+	int b1c = category_check();
+	vector<Stock> stock = Stock::read_data(b1c);
+	display_stock_table(stock);
+}
+
+void view_all_stock_items()
+{
+	vector<Stock> all_stock;
+	for (int ii = 1; ii < 11; ii++)
+	{
+		vector<Stock> temp = Stock::read_data(ii);
+		for (int ij = 0; ij < temp.size(); ij++)
+			all_stock.push_back(temp[ij]);
+	}
+	display_stock_table(all_stock);
+}
+
+void search_supply_item_by_name()
+{
+	vector<Supply> all_supply = Supply::supply_read_data();
+	string name;
+	cout << "\nEnter item Name\t\t: ";
+	getline(cin, name);
+	to_upper(name, 1);
+	vector<Supply> items;
+	for (auto i : all_supply)
+		if (i.get_supply_item_name() == name)
+			items.push_back(i);
+
+	if (items.size() == 0)
+		display_error("SD02");
+	else if (items.size() == 1)
+		items[0].show_data();
+	else
+		Supply::display_supply_table(items);
+}
+
+void add_supply_item()
+{
+	Supply temp;
+	temp.get_data();
+	cout << "\nPress [ENTER] to save or press [ESC] to cancel.\n\n";
+	char c = _getch();
+	if (c == 13)
+		temp.supply_write_data();
+}
 
 int main()
 {
@@ -32,15 +129,7 @@ int main()
 
 		display_header(username, user_position);
 		cout << "Main menu\n\n";
-		cout << "Select option number\n\n";
-		cout << "\t1. Stock\n";
-		cout << "\t2. Supply\n";
-		cout << "\t3. Staff\n";
-		cout << "\t4. Help\n";
-		cout << "\t5. Credits\n";
-		cout << "\t6. Log out\n";
-		cout << "\t7. Exit\n";
-		cout << endl;
+		display_main_menu();
 
 		char a = _getch();
 
@@ -52,15 +141,7 @@ int main()
 				{
 					display_header(username, user_position);
 					cout << "Main menu  >  Stock\n\n";
-					cout << "Select option\n\n";
-					cout << "\t1. View item details\n";
-					cout << "\t2. Add item\n";
-					cout << "\t3. Edit item\n";
-					cout << "\t4. Remove item\n";
-					cout << "\t5. Add promotion\n";
-					cout << "\t6. Transaction\n";
-					cout << "\t7. Go back to main menu\n";
-					cout << endl;
+					display_stock();
 
 					char b = _getch();
 
@@ -72,14 +153,7 @@ int main()
 							{
 								display_header(username, user_position);
 								cout << "Main menu  >  Stock  >  View item details\n\n";
-								cout << "Select option\n\n";
-								cout << "\t1. Search by item ID\n";
-								cout << "\t2. Search by item name\n";
-								cout << "\t3. Search by item category\n";
-								cout << "\t4. View all stock items\n";
-								cout << "\t5. Go back\n";
-								cout << endl;
-
+								display_view_item_details();
 								char b1 = _getch();
 
 								if (b1 == '1')
@@ -88,24 +162,7 @@ int main()
 									{
 										display_header(username, user_position);
 										cout << "Main menu  >  Stock  >  View item details  >  Search by item ID\n\n";
-										vector<Stock> all_stock;
-										for (int ii = 1; ii < 11; ii++)
-										{
-											vector<Stock> temp = Stock::read_data(ii);
-											for (int ij = 0; ij < temp.size(); ij++)
-												all_stock.push_back(temp[ij]);
-										}
-										bool not_found = 0;
-										string id;
-										Stock item;
-										cout << "\nEnter item ID\t\t: ";
-										getline(cin, id);
-										to_upper(id, 1);
-										item = Stock::find_by_id(id, all_stock, not_found);
-										if (not_found)
-											display_error("SD01");
-										else
-											item.show_data();
+										search_stock_item_by_id();
 									}
 									else
 										display_error("AM01");
@@ -117,28 +174,7 @@ int main()
 									{
 										display_header(username, user_position);
 										cout << "Main menu  >  Stock  >  View item details  >  Search by item name\n\n";
-										vector<Stock> all_stock;
-										for (int ii = 1; ii < 11; ii++)
-										{
-											vector<Stock> temp = Stock::read_data(ii);
-											for (int ij = 0; ij < temp.size(); ij++)
-												all_stock.push_back(temp[ij]);
-										}
-										string name;
-										cout << "\nEnter item Name\t\t: ";
-										getline(cin, name);
-										to_upper(name, 1);
-										vector<Stock> items = Stock::find_item(name, 1);
-										if (items.size() == 0)
-											display_error("SD02");
-										else if (items.size() == 1)
-											items[0].show_data();
-										else
-										{
-											Stock item;
-											Stock::find_name_and_display(item, all_stock, name, 1);
-											item.show_data();
-										}
+										search_stock_item_by_name();
 									}
 									else
 										display_error("AM01");
@@ -150,9 +186,7 @@ int main()
 									{
 										display_header(username, user_position);
 										cout << "Main menu  >  Stock  >  View item details  >  Search by item category\n\n";
-										int b1c = category_check();
-										vector<Stock> stock = Stock::read_data(b1c);
-										display_stock_table(stock);
+										search_stock_item_by_category();
 									}
 									else
 										display_error("AM01");
@@ -164,14 +198,7 @@ int main()
 									{
 										display_header(username, user_position);
 										cout << "Main menu  >  Stock  >  View item details  >  View all stock items\n\n";
-										vector<Stock> all_stock;
-										for (int ii = 1; ii < 11; ii++)
-										{
-											vector<Stock> temp = Stock::read_data(ii);
-											for (int ij = 0; ij < temp.size(); ij++)
-												all_stock.push_back(temp[ij]);
-										}
-										display_stock_table(all_stock);
+										view_all_stock_items();
 									}
 									else
 										display_error("AM01");
@@ -231,13 +258,7 @@ int main()
 							{
 								display_header(username, user_position);
 								cout << "Main menu  >  Stock  >  Add promotion\n\n";
-								cout << "Select option\n\n";
-								cout << "\t1. For an item\n";
-								cout << "\t2. For a brand\n";
-								cout << "\t3. For a category\n";
-								cout << "\t4. Go back\n";
-								cout << endl;
-
+								display_add_promotion();
 								char b5 = _getch();
 
 								if (b5 == '1')
@@ -294,12 +315,7 @@ int main()
 							{
 								display_header(username, user_position);
 								cout << "Main menu  >  Stock  >  Transaction\n\n";
-								cout << "Select option\n\n";
-								cout << "\t1. New transaction\n";
-								cout << "\t2. Transaction log\n";
-								cout << "\t3. Go back\n";
-								cout << endl;
-
+								display_transaction();
 								char b6 = _getch();
 
 								if (b6 == '1')
@@ -354,13 +370,7 @@ int main()
 				{
 					display_header(username, user_position);
 					cout << "Main menu  >  Supply\n\n";
-					cout << "Select option\n\n";
-					cout << "\t1. View supply details\n";
-					cout << "\t2. Add supply item\n";
-					cout << "\t3. Update stock\n";
-					cout << "\t4. Go back to main menu\n";
-					cout << endl;
-
+					display_supply();
 					char c = _getch();
 
 					if (c == '1')
@@ -371,12 +381,7 @@ int main()
 							{
 								display_header(username, user_position);
 								cout << "Main menu  >  Supply  >  View supply details\n\n";
-								cout << "Select option\n\n";
-								cout << "\t1. Search by name\n";
-								cout << "\t2. View all supply items\n";
-								cout << "\t3. Go back\n";
-								cout << endl;
-
+								display_view_supply_details();
 								char c1 = _getch();
 
 								if (c1 == '1')
@@ -385,25 +390,7 @@ int main()
 									{
 										display_header(username, user_position);
 										cout << "Main menu  >  Supply  >  View supply details  >  Search by name\n\n";
-										vector<Supply> all_supply = Supply::supply_read_data();
-
-										string name;
-										cout << "\nEnter item Name\t\t: ";
-										getline(cin, name);
-										to_upper(name, 1);
-
-										vector<Supply> items;
-
-										for (auto i : all_supply)
-											if (i.get_supply_item_name() == name)
-												items.push_back(i);
-
-										if (items.size() == 0)
-											display_error("SD02");
-										else if (items.size() == 1)
-											items[0].show_data();
-										else
-											Supply::display_supply_table(items);
+										search_supply_item_by_name();
 									}
 									else
 										display_error("AM01");
@@ -437,12 +424,7 @@ int main()
 						{
 							display_header(username, user_position);
 							cout << "Main menu  >  Supply  >  Add supply item\n\n";
-							Supply temp;
-							temp.get_data();
-							cout << "\nPress [ENTER] to save or press [ESC] to cancel.\n\n";
-							char c = _getch();
-							if (c == 13)
-								temp.supply_write_data();
+							add_supply_item();
 						}
 						else
 							display_error("AM01");
@@ -479,12 +461,7 @@ int main()
 				{
 					display_header(username, user_position);
 					cout << "Main menu  >  Staff\n\n";
-					cout << "Select option\n\n";
-					cout << "\t1. View staff details\n";
-					cout << "\t2. Add staff member\n";
-					cout << "\t3. Remove staff member\n";
-					cout << "\t4. Go back to main menu\n";
-					cout << endl;
+					display_staff();
 
 					char d = _getch();
 
